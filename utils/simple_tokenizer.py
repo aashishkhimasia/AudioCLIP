9c65b07f68aec943c085aeb83c8ad355aac57c6f
aashishkhimasia <72612451+aashishkhimasia@users.noreply.github.com>
2023-01-03 19:52:48 +0000

    Update simple_tokenizer.py
    merges = gzip.open("/assets/bpe_simple_vocab_16e6.txt.gz").read().decode("utf-8").split('\n')

--- utils/simple_tokenizer.py
+++ utils/simple_tokenizer.py
@@ -65,7 +65,7 @@ class SimpleTokenizer(object):
     def __init__(self, bpe_path: str = default_bpe()):
         self.byte_encoder = bytes_to_unicode()
         self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
-        merges = gzip.open(bpe_path).read().decode("utf-8").split('\n')
+        merges = gzip.open("/assets/bpe_simple_vocab_16e6.txt.gz").read().decode("utf-8").split('\n')
         merges = merges[1:49152-256-2+1]
         merges = [tuple(merge.split()) for merge in merges]
         vocab = list(bytes_to_unicode().values())
